b758dce35b8f7873b1d7d9b70a7aac0cb834fe63
Anirban Routh <anirbanrouth.dev@proton.me>
2024-03-08 16:57:54 +0530

    Login and Logout functionality added and is working flawlessly

--- dist/pages/logout.php
+++ dist/pages/logout.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h5>Session ID: <?php echo session_id(); ?></h5>    
-</body>
-</html>
-
 <?php
-session_start();
+// Start the session if it's not already started
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-echo "Logout page";
 // Unset all of the session variables
 $_SESSION = array();
 
